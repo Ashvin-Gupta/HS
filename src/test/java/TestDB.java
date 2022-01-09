1281faa12836f66e87be4b9ace3625ad284588cb
s4mwiniarski <sw3919@ic.ac.uk>
2022-01-09 00:17:17 +0000

    added first unit test using mockito - not passing yet.

--- src/test/java/TestDB.java
+++ src/test/java/TestDB.java
@@ -1,29 +1,47 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assertions.assertEquals;
 
 import Database.DatabaseConnection;
 import org.junit.jupiter.api.Test;
 
+import java.sql.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+//import org.junit.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 
 public class TestDB {
+
+    @InjectMocks private DatabaseConnection dbConn = new DatabaseConnection();
+    @Mock private Connection mockConnection = dbConn.getConnection();
+    @Mock private Statement mockStatement;
+
+    public TestDB() throws SQLException {
+        // ummm what do i put here lol
+    }
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    public void testConnection(){
-        DatabaseConnection dbConn = new DatabaseConnection();
-        Connection conn = dbConn.getConnection();
-
-        String usertext = "SamWiniarski";
-        String passwordtext = "TestPassword";
-
-        String sqlStr = "SELECT * FROM doctors WHERE id >0;";
-        ResultSet rset = s.executeQuery(sqlStr);
-
-        if (usernamedb.equals(usertext) && passwordnamedb.equals(passwordtext)) {
-            System.out.println("Log in successful");
-            validLogin = true;
-            break;
-        }
-
+    public void testMockDBConnection() throws Exception {
+        Statement m = mockConnection.createStatement();
+        String sqlStr = "SELECT * FROM doctors;";
+        Mockito.when(m).thenReturn(mockStatement);
+        Mockito.when(m.executeUpdate(Mockito.any())).thenReturn(1);
+        ResultSet valueSet = m.executeQuery(sqlStr);
+        Assertions.assertNotNull(valueSet,"it was null ... bruh ");
+        Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
     }
 }
+
+
+
