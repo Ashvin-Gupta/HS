b2541127feb7564cc11f029496bc8285939dff5f
apl22 <aidan.lees22@gmail.com>
2022-01-14 18:38:47 +0000

    getting patient info from database for filtered alerts

--- src/main/java/Views/AlertsPage.java
+++ src/main/java/Views/AlertsPage.java
@@ -9,6 +9,8 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AlertsPage implements Launchable {
@@ -27,6 +29,7 @@ public class AlertsPage implements Launchable {
 
     protected final Color BLUE  = new Color(37,78,112);
     protected final Color LBLUE = new Color(26,101,158);
+    protected final Color RED = new Color(195,60,86);
 
     public AlertsPage(int patientID) throws SQLException {
 
@@ -45,6 +48,10 @@ public class AlertsPage implements Launchable {
         displayAlertTable(patientID);
         displaytitle();
 
+        if (patientID != 0) {
+            getPatientInfo(patientID);
+        }
+
         backpanel = new JPanel();
         backpanel.setLayout(new BoxLayout(backpanel,BoxLayout.Y_AXIS));
 
@@ -62,6 +69,44 @@ public class AlertsPage implements Launchable {
         title.setFont(new Font("Roboto",Font.BOLD, 60));
         title.setForeground(BLUE);
         info.add(title);
+    }
+
+    private void getPatientInfo(int patientid) throws SQLException {
+        // patient name format
+        String getpatientname = "select name,surname,sex,age,blood from patients where id="+patientid;
+        PreparedStatement prpStm = conn.prepareStatement(getpatientname);
+        ResultSet rs = prpStm.executeQuery();
+        prpStm.close();
+        rs.next();
+
+        String patientname = rs.getString("name") + rs.getString("surname");
+        JLabel PatientName = new JLabel(patientname);
+        PatientName.setBounds((int) (WIDTH * 0.02), (int) (HEIGHT * 0.11), 400, 60);
+        PatientName.setFont(new Font("Roboto", Font.BOLD, 40));
+        PatientName.setForeground(RED);
+        info.add(PatientName);
+
+        String sexstring = rs.getString("sex");
+        JLabel sex = new JLabel("Sex: "+sexstring);
+        sex.setBounds((int) (WIDTH *0.7),(int) (HEIGHT *0.03),900,60);
+        sex.setFont(new Font("Roboto",Font.BOLD, 22));
+        sex.setForeground(Color.black);
+        info.add(sex);
+
+        String agestring = rs.getString("age");
+        JLabel age = new JLabel("Age: "+agestring);
+        age.setBounds((int) (WIDTH *0.7),(int) (HEIGHT *0.065),900,60);
+        age.setFont(new Font("Roboto",Font.BOLD, 22));
+        age.setForeground(Color.black);
+        info.add(age);
+
+        String bloodstring = rs.getString("blood");
+        JLabel blood = new JLabel("Blood: "+bloodstring);
+        blood.setBounds((int) (WIDTH *0.7),(int) (HEIGHT *0.1),900,60);
+        blood.setFont(new Font("Roboto",Font.BOLD, 22));
+        blood.setForeground(Color.black);
+        info.add(blood);
+
     }
 
     private void displayAlertTable(int patientID) throws SQLException {
