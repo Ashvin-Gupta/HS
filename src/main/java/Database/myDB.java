2cd58e10cf98ffe5b14041aafac62dd6f86e089b
apl22 <aidan.lees22@gmail.com>
2022-01-15 13:20:36 +0000

    rollback

--- src/main/java/Database/myDB.java
+++ src/main/java/Database/myDB.java
@@ -47,6 +47,7 @@ public class myDB {
         String HRString2 = readIn("HeartRate_2.txt");
         String HRString3 = readIn("HeartRate_3.txt");
 
+
         String sqlStr = "create table patients ( id SERIAL PRIMARY KEY, name varchar(20) NOT NULL, surname varchar(20) NOT NULL, sex varchar(20), age int, blood varchar (4), sbp varchar(8000), dbp varchar(8000), rr varchar(8000), hr varchar(8000), temp varchar(8000), ecg varchar(8000));";
         String sqlStr1 = "insert into patients  values (1, 'Antonio', 'Serra', 'Male', 21, 'B+',"+quotes+SPString1+quotes+","+quotes+DPString1+quotes+","+quotes+RRString1+quotes+","+quotes+HRString1+quotes+","+quotes+TString1+quotes+","+quotes+ECGString1+quotes+");";
         String sqlStr2 = "insert into patients  values (2, 'Tom', 'Lee', 'Male', 34, 'A',"+quotes+SPString2+quotes+","+quotes+DPString2+quotes+","+quotes+RRString2+quotes+","+quotes+HRString2+quotes+","+quotes+TString2+quotes+","+quotes+ECGString2+quotes+");";
